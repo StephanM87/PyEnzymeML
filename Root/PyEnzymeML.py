6c27c59ca6ac26baa21db672ed3f521802b45fb5
s.malzacher <s.malzacher@fz-juelich.de>
2021-01-25 14:54:26 +0100

    neue Dokumentation

--- Root/PyEnzymeML.py
+++ Root/PyEnzymeML.py
@@ -8,32 +8,46 @@ doc = EnzymeMLDocument("Test", 3, 2)
 
 # Create Vessel
 
-vessel = Vessel("name", "v0", "meta_v0", True, 2.0, "nmol/l")
+vessel = Vessel("name", "v0", 2.0, "nmole/l")
+
+#reactant = Protein()
+
+
 
 doc.setVessel(vessel)
 
 # Create Reactant
 
-reactant = Reactant("pyruvat", "r0", "meta_r0", "SBO:0003", "v0", 1.0, "mmol/l", False, False)
-doc.addReactant(reactant)
+reactant = Reactant("pyruvat", "v0", 1.0, "mmole/l", False)
+pyruvat = doc.addReactant(reactant)
+print(pyruvat)
+
+
+# Puffer wird einfach als Reactant hinzugefügt
 
 # add Reaction
 
 # educts, product, modifiers are not mandatory
 
-reaction = EnzymeReaction(37.0, "°C", 6.0, "Reeaktion1", True, "reac0", "meta_reac0")
+reaction = EnzymeReaction(37.0, "C", 6.0, "Reeaktion1", True)
 
 
 # addEduct : replicates are not madatrory
 # Educts need to be defined previously as reactant
 
-reaction.addEduct("r0", 1.0, False, doc)
+reaction.addEduct(pyruvat, 1.0, False, doc)
 
-replicate = Replicate("Repl1", "r0", "conc", "nmol/l", "s")
+replicate = Replicate("Repl1", pyruvat, "conc", "nmole/l", "s", 1.0)
 series = pd.Series([1,2,3])
 series.Index = [1,2,3]
 replicate.setData(series)
-reaction.addReplicate(replicate)
+reaction.addReplicate(replicate, doc)
+
+
+
+# mit reaction.exportReplicates() zieht man sich die Messdaten aus dem Omex archive
+
+# mit doc.printReaction() zeiht man die Infos über die Reaktanden aus dem Omex file raus
 
 
 
--- Root/new.py
+++ Root/new.py
@@ -1,3 +1,8 @@
 from pyenzyme.enzymeml.tools import EnzymeMLReader
 
 doc = EnzymeMLReader().readFromFile("Modified.omex", omex=True)
+
+protein = doc.getProteinDict()
+
+print(protein)
+
